d9afd4672e6e8861774dcb29f3b54baa0773842f
Niels <7136117+TheDutchDev@users.noreply.github.com>
2026-02-04 15:48:21 +0100

    Fix clockspeed

--- HAL/STM32F411/src/System.cpp
+++ HAL/STM32F411/src/System.cpp
@@ -19,7 +19,7 @@ static void SystemClock_Config() {
     RCC_OscInitStruct.HSEState = RCC_HSE_ON;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLM = 25;
     RCC_OscInitStruct.PLL.PLLN = 200;
     RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
     RCC_OscInitStruct.PLL.PLLQ = 4;
